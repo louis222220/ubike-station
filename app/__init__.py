8ea5b3a95590985f106fa1e86e5a5d8263308f69
Louis Su <louis222220@gmail.com>
2018-11-10 18:35:12 +0800

    Refactor favorite module with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-from .routes import station
-# from .routes import favorite
-from .routes import hello
 from config import config
 
 db = SQLAlchemy()
@@ -22,10 +18,9 @@ def create_app(config_name):
         from .modules.station import check_init_stations
         check_init_stations()
 
-    # app.register_blueprint(station.bp)
-    # app.register_blueprint(favorite.bp)
-
-    # FIXME:
-    app.register_blueprint(hello.bp)
+    from .routes import station
+    from .routes import favorite
+    app.register_blueprint(station.bp)
+    app.register_blueprint(favorite.bp)
 
     return app
--- app/routes/favorite.py
+++ app/routes/favorite.py
@@ -1,61 +1,63 @@
-import functools
-
-from flask import Blueprint, jsonify
-from werkzeug.exceptions import abort
-import json
-
-from ubike.db import sqlite
-from ubike.modules.station import is_station
+from flask import request, Blueprint, jsonify
+from datetime import datetime
+from ..__init__ import db
+from ..models import FavoriteStation
+from ..modules.station import is_station
 
 bp = Blueprint('favorite', __name__, url_prefix='/favorite')
 
 
 @bp.route('/')
 def index():
-    favorite_station_rows = sqlite.db.execute(
-        'SELECT stationNo FROM favoriteStation'
-        ' ORDER BY stationNo ASC'
-    ).fetchall()
+    favorite_station_rows = FavoriteStation.query \
+        .order_by(FavoriteStation.station_id.asc())
 
-    favorite_stations = [row['stationNo'] for row in favorite_station_rows]
+    favorite_stations = [
+        favorite_station.station_id
+        for favorite_station in favorite_station_rows]
     return jsonify({"favorite_stations": favorite_stations})
 
 
-@bp.route('/insert/<int:stationNo>')
-def insertFavoriteStation(stationNo):
-    if not is_station(stationNo):
-        return "station {} doesn't exist" . format(stationNo)
-    else:
-        if sqlite.db.execute(
-            'SELECT * FROM favoriteStation WHERE stationNo = ?',
-            (stationNo, )
-        ).fetchone() is not None:
-            return "station {} already in table" . format(stationNo)
+# TODO: refactor to be cleaner
+@bp.route('/', methods=['POST'])
+def insertFavoriteStation():
+    new_station_id = request.json.get('station_id')
 
-        sqlite.db.execute(
-            'INSERT INTO favoriteStation (stationNo)'
-            ' VALUES (?)',
-            (stationNo, )
+    if not is_station(new_station_id):
+        # TODO: use json in every return
+        return "station {} doesn't exist" . format(new_station_id)
+    else:
+        favorite_station = FavoriteStation.query \
+            .filter_by(station_id=new_station_id).first()
+        if favorite_station is not None:
+            return "station {} already in table" . format(new_station_id)
+
+        # TODO: time zone
+
+        new_favorite = FavoriteStation(
+            station_id=new_station_id,
+            created_time=datetime.now()
         )
-        sqlite.db.commit()
+        db.session.add(new_favorite)
+        db.session.commit()
+
         return "success in inserting"
 
 
-@bp.route('/delete/<int:stationNo>')
-def deleteFavoriteStation(stationNo):
-    if not is_station(stationNo):
-        return "station {} doesn't exist" . format(stationNo)
-    else:
-        if sqlite.db.execute(
-            'SELECT * FROM favoriteStation WHERE stationNo = ?',
-            (stationNo, )
-        ).fetchone() is None:
-            return "station {} is not in table" . format(stationNo)
+@bp.route('/', methods=['DELETE'])
+def deleteFavoriteStation():
+    to_delete_station_id = request.json.get('station_id')
 
-        sqlite.db.execute(
-            'DELETE FROM favoriteStation'
-            ' WHERE stationNo = ?',
-            (stationNo, )
-        )
-        sqlite.db.commit()
+    if not is_station(to_delete_station_id):
+        return "station {} doesn't exist" . format(to_delete_station_id)
+    else:
+        favorite_station = FavoriteStation.query \
+            .filter_by(station_id=to_delete_station_id).first()
+        if favorite_station is None:
+            return "station {} is not in table" . format(to_delete_station_id)
+
+        FavoriteStation.query.filter_by(station_id=to_delete_station_id) \
+            .delete()
+        db.session.commit()
+
         return "success in deleting"
